73141e84b21196736962e5146f57178a4a2936e6
manoj shetty <manoj.shetty@getvymo.com>
2025-04-10 14:13:32 +0530

    Commit

---
+++ CSharpNew/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+   Console.WriteLine("Enter the Input for String : ");
+ String Input = Console.ReadLine();
+String Reverse = ReverseString(Input);
+Console.WriteLine("The Reverse String value is : " + Reverse);
+
+String ReverseString(String Str)
+{
+
+    String Reverse = "";
+
+    foreach (char item in Str)
+    {
+        Reverse = item + Reverse;
+
+    }
+    return Reverse;
+
+}
+
+
+
---
+++ CSharpNew/Test2.cs
@@ -0,0 +1,38 @@
+﻿// Program for reverse number
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpNew
+{
+    internal class Test2
+    {
+
+        static void Main()
+        {
+
+            Console.WriteLine("Enter the No for reverse: ");
+            String inputNumber = Console.ReadLine();
+            String revereversedNumber = ReverseNumber(inputNumber);
+            Console.WriteLine("Result after reverse :" + revereversedNumber);
+
+        }
+
+        static String ReverseNumber(String Manu)
+        {
+            String reverse = "";
+
+            foreach (char c in Manu)
+            {
+                reverse = c + reverse;
+
+            }
+            return reverse;
+
+        }
+    }
+    
+}
